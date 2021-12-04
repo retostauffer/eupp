5ffd8e7c84a9fceac9f395203421123fafacf00b
Reto Stauffer <reto.stauffer@uibk.ac.at>
2021-12-04 18:26:51 +0100

    fix: Updating url in onAttach message

--- R/onAttach.R
+++ R/onAttach.R
@@ -8,9 +8,8 @@
 
     # Terms of use
     tos <- "
-    By downloading data from this dataset, you agree to the terms and conditions defined at
-    https://github.com/Climdyn/climetlab-eumetnet-postprocessing-benchmark/blob/main/LICENSE and
-    https://github.com/Climdyn/climetlab_eumetnet_postprocessing_benchmark/blob/main/DATA_LICENSE
+    By downloading data from this dataset, you agree to the terms and conditions defined in
+    https://github.com/retostauffer/eupp/blob/main/README.md
     If you do not agree with such terms, do not download the data.
     \n"
     cat(tos)
