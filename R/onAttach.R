b72b82f7b480105314f6653b0f107ee7fec0150a
Reto Stauffer <reto.stauffer@uibk.ac.at>
2021-12-06 18:19:26 +0100

    fix: Prettifying output

--- R/onAttach.R
+++ R/onAttach.R
@@ -12,7 +12,6 @@
     tos <- "
     By downloading data from this dataset, you agree to the terms and conditions defined in
     https://github.com/retostauffer/eupp/blob/main/README.md
-    If you do not agree with such terms, do not download the data.
-    \n"
+    If you do not agree with such terms, do not download the data.\n"
     packageStartupMessage(tos)
 }
