2abcefa36cf34bb0e90f4dad5c717a1ee7aadfee
Reto Stauffer <reto.stauffer@uibk.ac.at>
2022-01-04 19:29:57 +0100

    hm: rethink this subsetting bilinear strategy once again

--- R/eupp_stars.R
+++ R/eupp_stars.R
@@ -12,18 +12,47 @@
 #'
 #' @author Reto Stauffer
 #' @rdname eupp_stars
-#' @export
 "[.eupp_stars" <- function(x, ...) {
-    x <- NextMethod(x)
+    x <- NextMethod(x, ...)
     class(x) <- c("eupp_stars", class(x))
     return(x)
 }
 
 #' @author Reto Stauffer
 #' @rdname eupp_stars
-#' @export
 "[[.eupp_stars" <- function(x, ...) {
-    x <- NextMethod(x)
+    x <- NextMethod(x, ...)
     class(x) <- c("eupp_stars", class(x))
     return(x)
+}
+
+
+#' @author Reto Stauffer
+#' @importFrom stars st_extract
+#' @rdname eupp_stars
+#' @export
+st_extract.eupp_stars <- function(x, at, bilinear = FALSE, ...) {
+    stopifnot(isTRUE(bilinear) | isFALSE(bilinear))
+
+    #x <- NextMethod(x, bilinear = bilinear, ...)
+    if (!bilinear) {
+        print('xxxxxxxxxx')
+        res <- NextMethod(x, at = at, bilinear = bilinear, ...)
+    } else {
+        res <- list()
+        (idx_number <- which(attr(attr(x, "dimensions"), "names") == "number"))
+        (idx_time   <- which(attr(attr(x, "dimensions"), "names") == "time"))
+        if (!length(idx_number) == 0 && !length(idx_time) == 0) {
+            ndim <- length(dim(x))
+            pat  <- paste(ifelse(seq(0, ndim) == idx_time, "%d", ""), collapse = ",")
+            for (i in seq_len(dim(x)[idx_time])) {
+                tmp <- sprintf("x[%s, drop = TRUE]", sprintf(pat, i))
+                print(tmp)
+                tmp <- eval(parse(text = tmp))
+                res <- c(res, stars::st_extract(tmp, at = at, bilinear = bilinear, ...))
+            }
+        }
+    }
+    return(res)
+
 }
